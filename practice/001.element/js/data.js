f9d9e48de19a701aa5366440de62f148cd09ef13
yongzhibufen <1648341379@qq.com>
2017-10-14 14:51:58 +0800

    Initial commit

---
+++ practice/001.element/js/data.js
@@ -0,0 +1,28 @@
+;(function(){
+	'use strict'
+
+	window.user_list = [
+	{
+		name: '路人甲',
+		game: ['cs','cf','lol'],
+	},
+	{
+		name: '路人乙',
+		game: ['dota','cs','rl']
+	},
+	{
+		name: '路人丙',
+		game: ['dota','cs','rl']
+	},
+	{
+		name: '路人丁',
+		game: ['dota','ow','war']
+	},
+	{
+		name: '路人戊',
+		game: ['ow','war','rl']
+
+	},
+
+	];
+})();
---
+++ practice/001.element/main.js
@@ -0,0 +1,59 @@
+;(function(){
+  'use strict'
+
+  var result = user_list;
+  var card_list = document.querySelector('#card-list');
+
+//筛选数据
+  function search(keyword){
+    //清空结果
+    result = [];                  
+    for (var i = 0; i < user_list.length; i++) {
+      var user = user_list[i];
+      //indecOf方法更好一些
+      if (user.name.indexOf(keyword) != -1) {
+        result.push(user);
+      }
+    }
+  }
+
+//渲染
+
+  function render(){
+    //清空遗留的渲染
+    card_list.innerHTML = '';
+    for (var i = 0; i < result.length; i++) {
+      var user = result[i]
+      var card = document.createElement('div');
+      card.innerHTML = `<div><h4>${user.name}</h4>: <h4>${user.game}</4></div>`;
+      card_list.appendChild(card);
+    }
+  }
+
+//程序入口
+
+  function boot(){
+    // 将最开始的数据渲染到页面
+    render();
+    var form = document.querySelector('form');
+    var data = document.getElementById('api-input')
+    var keyword;
+    
+    form.addEventListener('submit',function(e){
+      e.preventDefault();
+      keyword = data.value;
+      //控制输入
+      if (!keyword) {
+        alert('请输入关键词');
+        return;
+      }
+      search(keyword);
+      render();
+    })
+
+
+  }
+
+  //开始调用
+  boot();
+})();
